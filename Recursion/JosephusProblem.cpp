584ebe072799c15e95c6d563543360eeec1cbc4e
AnujWebDev <anujyash10gmail.com>
2025-01-19 20:48:22 +0530

    Add comments

--- Recursion/JosephusProblem.cpp
+++ Recursion/JosephusProblem.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+//This is the same problem solving for predict the Winner In customised time in O(n)
 int Winner(int n,int k){
     if(n == 1) return 0;
     return (Winner(n-1,k)+k)%n;
