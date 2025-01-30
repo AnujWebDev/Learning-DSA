7da1f8f966f5c6662a5ebba8c095c60e2b040e62
AnujWebDev <anujyash10gmail.com>
2025-01-30 23:22:06 +0530

    Learned how to make Abstract Class in OOPS

--- OOPS/PolymorphismVirtualFunction.cpp
+++ OOPS/PolymorphismVirtualFunction.cpp
@@ -4,9 +4,10 @@ using namespace std;
 //THis is How virtual Function Works in Runtime;
 class Animal{
     public:
-    virtual void speak(){
-        cout<<"Hu hu \n";
-    }
+    // virtual void speak(){
+    //     cout<<"Hu hu \n";
+    // }
+    virtual void speak()= 0; //Extract Function or Abstract Class Now i want to Create Object For Speak
 };
 class Dog : public Animal {
     public:
@@ -28,7 +29,7 @@ int main(){
     Animal *p;
     vector<Animal*>animals;
     animals.push_back(new Dog());
-    animals.push_back(new Animal());
+    // animals.push_back(new Animal()); //Now i dont want to Make this 
     animals.push_back(new Cat());
     animals.push_back(new Dog());
     animals.push_back(new Cat());
