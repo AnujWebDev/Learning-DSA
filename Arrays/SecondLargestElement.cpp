874f81ff927323a6feb4de185a7d6d2313e26c7b
AnujWebDev <anujyash10gmail.com>
2025-02-14 22:19:56 +0530

    Learned Basics of Array

---
+++ Arrays/SecondLargestElement.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+//Find the second largest element in an array of unique elements of size n. Where n>3.
+
+int main(){
+    int n;
+    cout<<"Enter Your Size: ";
+    cin>>n;
+    vector<int>arr(n);
+    cout<<"Enter Your Array Elements: ";
+    for(int i = 0; i < n;i++){
+        cin>>arr[i];
+    }
+    int ans = INT_MIN;
+    for(int i = 0; i < n;i++){
+        if(ans < arr[i]) ans = arr[i];
+    }
+    int second = INT_MIN;
+    for(int i = 0; i < n;i++){
+        if(ans != arr[i]){
+            if(second < arr[i]) second = arr[i];
+        }
+    }
+    cout<<second;
+    
+    return 0;
+}
