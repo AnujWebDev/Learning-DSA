bfe20a6b6a0c841e2bad1596daebb50ec1ad87c6
AnujWebDev <anujyash10gmail.com>
2025-03-05 23:41:48 +0530

    Solved One more Question Related To Binary Search

---
+++ Arrays/FindPeakElement2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int findPeakElement(int nums[],int n) {
+    int start = 0, end = n - 1, mid;
+
+    while (start < end) {
+        mid = start + (end - start) / 2; 
+        
+        if (nums[mid] > nums[mid + 1]) {
+            end = mid;
+        } else {
+            start = mid + 1; 
+        }
+    }
+    return start;
+}
+int main(){
+    int arr[] = {1,2,1,3,5,6,4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<findPeakElement(arr,n);
+    return 0;
+}
