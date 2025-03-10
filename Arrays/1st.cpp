3b0b6b62004c31f7fad8d1eb97650c3a8c8a0e92
AnujWebDev <anujyash10gmail.com>
2025-03-10 23:17:38 +0530

    Add commenting

--- Arrays/1st.cpp
+++ Arrays/1st.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
-
+//I am going to start Array Topoc for DSA
 int main(){
     int arr[5] = {1,2,3,4,5};
-    for(int i = 0; i < 5;i++){
+    for(int i = 0; i < 5;i++){  
         cout<<arr[i]<<endl;
     }
 }
