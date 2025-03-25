f288e038384c83fff7fe48b83138609c20413194
AnujWebDev <anujyash10gmail.com>
2025-03-25 23:29:25 +0530

    Added Commenting

--- Arrays/BinarySearch.cpp
+++ Arrays/BinarySearch.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-//This code snippet refers to Binary Search
+//This code snippet refers to Binary Search of given array
 int main(){
     int arr[] = {1,2,3,4,5},target = 1;
     int size = sizeof(arr)/sizeof(arr[0]);
