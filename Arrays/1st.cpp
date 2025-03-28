654c14d94e626067497e4d5883bbb3bc75c38597
AnujWebDev <anujyash10gmail.com>
2025-03-28 23:43:05 +0530

    Added Commenting

--- Arrays/1st.cpp
+++ Arrays/1st.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-//I am going to start Array Topoc for DSA Covering All The Topics
+//I am going to start Array Topoc for DSA Covering All The Topics..This is Just the a start
 int main(){
     int arr[5] = {1,2,3,4,5};
     for(int i = 0; i < 5;i++){  
